a305ed5871fc4c78c82d27ba0a771a376b6a39ef
quarkusio <no-reply@quarkus.io>
2022-07-13 21:22:21 +0000

    Initial commit

---
+++ src/test/java/org/fresh/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.fresh;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
